9cb7dd7eb2d472661972f2ac78cefdcb76c331cd
MusicTastingClubNU <musictastingclub@gmail.com>
2025-02-04 21:22:36 -0600

    adding to pickData.json

--- src/components/SP/ThisWeeksPicks.tsx
+++ src/components/SP/ThisWeeksPicks.tsx
@@ -8,7 +8,7 @@ import YsAlbumArt from "../../imgs/manualAlbumArt/WQ24_W2_AotW.png";
 export default function ThisWeeksPicks() {
   const isMobile = useMediaQuery("(max-width: 768px)");
   const currQuarter = "WQ25";
-  const currWeek = "WEEK 4";
+  const currWeek = "WEEK 5";
 
   return (
     <div className={isMobile ? "faq2" : "faq"}>
